5c8023c1e3afa0973cc85ed1e3cbc313acf1c322
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-23 19:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
-var array=[]
+
 function theBeatlesPlay(arrayMusicians, arrayInstruments){
-  
+var array=[]  
   
     for (i=0;i<arrayMusicians.length; i++){
       array.push(arrayMusicians[i]+"plays the "+arrayInstruments[i])
