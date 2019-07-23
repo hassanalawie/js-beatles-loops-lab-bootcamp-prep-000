9f9ba64749984823f03d7f8e6e12239731da356e
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-23 19:23:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments){
 var array=[]  
   
     for (var i = 0 ; i < arrayMusicians.length ; i++){
-      array.push(arrayMusicians[i]+ "plays " + arrayInstruments[i])
+      array.push(arrayMusicians[i]+ " plays " + arrayInstruments[i])
     }
     return array 
 }
